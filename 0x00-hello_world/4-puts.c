e6aa1e6a1e4f18dc309cc5279b3216b4069b74a9
Eslam <islamnader34@yahoo.com>
2023-06-15 11:24:38 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-int main (void)
+int main(void)
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+   puts("\"Programming is like building a multilingual puzzle");
 
-return 0;
+   return (0);
 
 }
