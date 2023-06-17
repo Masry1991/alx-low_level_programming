eb1a95a40c491a7d11662023196b663dbed3e79c
Eslam <islamnader34@yahoo.com>
2023-06-17 04:30:05 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Describtion: program that prints 0-9
+ * Return: 0 Always (Success)
+*/
+int main(void)
+{
+int x = 0;
+while (x <= 9)
+{
+printf("%i, x");
+x++;
+}
+printf("\n");
+return (0);
+}
