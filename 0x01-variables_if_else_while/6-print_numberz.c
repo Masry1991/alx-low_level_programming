c53230d5095f5f7af49dc9c4acdf483b158828a0
Eslam <islamnader34@yahoo.com>
2023-06-17 05:41:32 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Describtion: program that prints 0-9
+ * Return: 0 Always (Success)
+*/
+int main(void)
+{
+int d;
+for (d = 0, 'd' < '10', d++;)
+putchar(d);
+putchar('\n');
+return (0);
+}
