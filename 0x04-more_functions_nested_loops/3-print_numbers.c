e5722736be18d71fb4776ebc7f6f6d4523296002
Eslam <islamnader34@yahoo.com>
2023-06-22 12:59:54 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *                 only using _putchar twice
+ *
+ * Return: Always 0
+*/
+
+void print_numbers(void)
+{
+int x = 0;
+while (x <= 9)
+_putchar('x');
+_putchar('\n');
+return (0);
+}
