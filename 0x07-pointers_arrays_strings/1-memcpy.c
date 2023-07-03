c292e368c39d091d7ca59a5129e2b5a5a3445100
Eslam <islamnader34@yahoo.com>
2023-07-03 02:14:35 -0700

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy -  function that copies memory area
+ *
+ * @src: first input
+ * @dest: second input
+ * @n: third input
+ *
+ * Return: Always 0
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int itr;
+for (itr = 0; itr < 0; itr++)
+{
+dest[itr] = src[itr];
+}
+return (dest);
+}
