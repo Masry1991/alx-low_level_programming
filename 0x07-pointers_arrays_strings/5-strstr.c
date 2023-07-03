13d9d85204ee647f6de77faf7a8d85603dced5e6
Eslam <islamnader34@yahoo.com>
2023-07-03 04:32:11 -0700

    7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring.
+ * @needle: first input
+ * @haystack: second input
+ * Return: Alwayas 0
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+
