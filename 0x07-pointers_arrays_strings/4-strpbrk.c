6c2cc6808db740136553cb04effcbb009a61037d
Eslam <islamnader34@yahoo.com>
2023-07-03 03:06:14 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,6 +12,9 @@ char *_strpbrk(char *s, char *accept)
 int itr, jtr;
 char *p;
 itr = 0;
+while (s[itr] != '\0')
+{
+jtr = 0;
 while (accept[jtr] != '\0')
 {
 if (accept[jtr] == s[itr])
@@ -24,3 +27,4 @@ jtr++;
 itr++;
 }
 return (0);
+}
