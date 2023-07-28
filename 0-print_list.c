80cb2881657d95c9a7f4f1d27fe96f1de029a055
Eslam <islamnader34@yahoo.com>
2023-07-28 12:25:05 -0700

    0

---
+++ 0-print_list.c
@@ -0,0 +1,23 @@
+#include "LISTS_H"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements
+ * @h: Pointer
+ * Return: Number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t s = 0;
+while (h)
+{
+if (!h->str)
+printf("[0] (nill)\n");
+else
+printf("[%u]%s\n", h->len, h->str);
+h = h->next;
+s++;
+}
+return (s);
+}
